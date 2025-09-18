69a89899618c82e6ab0c18916660948b952cf383
Kévin SPINICCI <kevin.spinicci@outlook.com>
2025-09-18 17:22:25 +0900

    responsive navbar pour les petits écrans

---
+++ main.js
@@ -0,0 +1,26 @@
+/** =============================================================================
+ * Main script for interactive website.
+ * Author: Kévin SPINICCI
+ * Data: 2025-09-18
+ ============================================================================= */
+
+/**
+ * Show/hide the navbar when the navbar toggle button is pressed.
+ * Hide the navbar when an element is pressed.
+ * @returns void
+ */
+function toggleNavbar() {
+    var navbar = document.getElementById("nav-bar");
+    var navToggleIcon = document.getElementById("nav-toggle-icon");
+    /* Only trigger toggle event if screen size is smaller than 800px */
+    if ( !matchMedia("(max-width: 800px)").matches )  {
+        return;
+    }
+    if (navbar.className === "nav-bar") {
+        navbar.className += " compact";
+        navToggleIcon.innerHTML = "close"
+    } else {
+        navbar.className = "nav-bar";
+        navToggleIcon.innerHTML = "menu"
+    }
+}
